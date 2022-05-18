864bc9ac0f05cdb9cd3c36e80f51ba7f029b32eb
Darlon Henrique <darlonhenrisouza@gmail.com>
2022-05-18 15:06:58 -0300

    build(dependencie): add sass-loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin')
 const isDevelopment = process.env.NODE_ENV !== 'development'
 
 module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
+  mode: isDevelopment ? 'development' : 'production', // if NODE_ENV is different of development, set mode for production, if it equals development, set for development
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -13,14 +13,21 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
   devServer: {
-    static: path.resolve(__dirname, 'public')
+    static: path.resolve(__dirname, 'public') // dev server for improve development ux
   },
-  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map', // souce-map for dev and prod
   plugins: [
     new htmlWebpackPlugin({
+      // generate index.html file dynamically
       template: path.resolve(__dirname, 'public', 'index.html')
     })
   ],
+  performance: {
+    // avoid size limits
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   module: {
     rules: [
       {
@@ -33,11 +40,13 @@ module.exports = {
       },
       {
         // loader for css
-        test: /\.css$/,
+        test: /\.scss$/,
         exclude: /node_modules/,
-        use: {
-          loader: ['style-loader', 'css-loader']
-        }
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
       }
     ]
   }
